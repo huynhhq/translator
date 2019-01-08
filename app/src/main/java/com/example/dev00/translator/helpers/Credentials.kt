cd6fa54f5f8976d08e47ca5b5ec6f41aff384dff
Hoang Quoc Huynh <hoang_quoc_huynh@wae.jp>
2019-01-08 17:45:20 +0700

    Delete Google Api Key

--- app/src/main/java/com/example/dev00/translator/helpers/Credentials.kt
+++ app/src/main/java/com/example/dev00/translator/helpers/Credentials.kt
@@ -12,7 +12,7 @@ class Credentials {
         val APP_KEY = "1f49bae741351a2c831dede3dcbbe2da14dcbf22c6a8e26cc9ad82e1090f5a295814109a4a2ac6cd47b4b0236f92fd846ebca6356488a12d35230e3e3369678b"
         val PCM_FORMAT = PcmFormat(PcmFormat.SampleFormat.SignedLinear16, 16000, 1)
         val YANDEX_KEY = "trnsl.1.1.20181206T043553Z.e749c47e3c3f08f3.3c2eceb80225b69ce6c4168bfcc526218842c890"
-        val GOOGLE_KEY = "AIzaSyABqqm2ABW32J13MpsjTC7Y--Nql6HWfQE"
+        val GOOGLE_KEY = ""
         val BASE_ADDRESS_YANDEX = "https://translate.yandex.net"
         val BASE_ADDRESS_GOOGLE = "https://translation.googleapis.com"
     }
